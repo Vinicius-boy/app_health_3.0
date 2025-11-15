370719f718b30920232315d16d29893247d1141a
Wissenschaftler <desouzaboyvinicius@gmail.com>
2025-11-15 19:32:34 -0300

    rebirth

--- main_tkinter.py
+++ main_tkinter.py
@@ -9,17 +9,10 @@ from tkinter import messagebox, ttk
 from view_tkinter.menu_tkinter import show_main_menu
 
 def main():
-    # Initialize database 
+    # Initialize database (código original mantido)
     patient_database.init_database()
-
-    window = tk.Tk()
-
-
-    window.title("Unesc Saúde")
-    window.geometry("400x400")
-    window.resizable(False, False)
     
-
+    # Inicia interface Tkinter
     show_main_menu()
 
 if __name__ == '__main__':
